8b9699f83152a4e668edb0265404c485721acc5d
liberalman <zscchina@163.com>
2018-04-24 11:32:44 +0800

    use docker

--- api.php
+++ api.php
@@ -8,7 +8,7 @@
  * 默认编码：UTF-8
  */
 // 加载 XS 入口文件
-require_once '/usr/local/xunsearch/sdk/php/lib/XS.php';
+require_once 'sdk/php/lib/XS.php';
 error_reporting(E_ALL ^ E_NOTICE);
 
 $xs = new XS('libertyblog'); // 建立 XS 对象，项目名称为：libertyblog
